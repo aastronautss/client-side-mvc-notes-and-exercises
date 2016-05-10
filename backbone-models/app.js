14df5f1539dbb88d0ea508542fbd93435120ad2c
Tyler Guillen <tyguillen@gmail.com>
2016-05-10 17:59:02 -0400

    finish backbone models exercises

--- backbone-models/app.js
+++ backbone-models/app.js
@@ -11,3 +11,49 @@ var my_post = new Post({
 });
 
 console.log(my_post.toJSON());
+
+// 3.
+
+function logModel(model) {
+  console.log(model.toJSON());
+}
+
+my_post.fetch({
+  success: logModel
+});
+
+// 4.
+
+var User = Backbone.Model.extend({
+  urlRoot: "http://jsonplaceholder.typicode.com/users"
+});
+
+var my_user = new User({
+  id: my_post.get("userId")
+});
+
+my_user.fetch({
+  success: logModel
+});
+
+// 5.
+
+my_post.set("user", my_user);
+logModel(my_post);
+
+// 6.
+
+Post = Backbone.Model.extend({
+  urlRoot: "http://jsonplaceholder.typicode.com/posts",
+  setUser: function() {
+    var self = this,
+        user = new User({ id: self.get("userId") });
+
+    user.fetch({
+      success: function(model) {
+        self.set("user", model);
+        logModel(self);
+      }
+    });
+  }
+});
